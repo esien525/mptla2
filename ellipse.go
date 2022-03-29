72811559dd5894f2f3dcfc5f003caac40b39a0da
ESien <esien525@gmail.com>
2022-03-29 15:34:15 +0800

    fix bug

--- ellipse.go
+++ ellipse.go
@@ -10,5 +10,5 @@ type Init struct {
 }
 // Get Eccentricity of Ellipse
 func (e *Init) GetEccentricity() float64 {
- return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / A
+ return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / e.A
 }
